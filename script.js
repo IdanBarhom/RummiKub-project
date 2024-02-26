af7b05c76074de8fe494ecaca8ba658aba817197
Idan Barhom <130668498+IdanBarhom@users.noreply.github.com>
2024-02-26 18:44:53 +0200

    combine the html to one file

---
+++ script.js
@@ -0,0 +1,12 @@
+function verify(){
+    console.log("entered")
+    document.getElementById('main').style.visibility="hidden"
+    document.getElementById('game').style.visibility="visible"
+
+
+}
+
+function exit(){
+    document.getElementById('game').style.visibility="hidden"
+    document.getElementById('main').style.visibility="visible"
+}
