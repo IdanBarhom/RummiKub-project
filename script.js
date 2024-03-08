c2d7c0d49153e37a043cc4289ab6a3256bcce253
Idan Barhom <130668498+IdanBarhom@users.noreply.github.com>
2024-03-09 00:34:55 +0200

    changed the cards array, fixed and added sort 789,777 and draw card

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 
 
 let n; /*number of player*/
-let cardsArray=["1-01","1-02","1-03","1-04","1-05","1-06","1-07","1-08","1-09","1-10","1-11","1-12","1-13",
-"2-01","2-02","2-03","2-04","2-05","2-06","2-07","2-08","2-09","2-10","2-11","2-12","2-13",
-"3-01","3-02","3-03","3-04","3-05","3-06","3-07","3-08","3-09","3-10","3-11","3-12","3-13",
-"4-01","4-02","4-03","4-04","4-05","4-06","4-07","4-08","4-09","4-10","4-11","4-12","4-13",
-"0","1-01","1-02","1-03","1-04","1-05","1-06","1-07","1-08","1-09","1-10","1-11","1-12","1-13",
-"2-01","2-02","2-03","2-04","2-05","2-06","2-07","2-08","2-09","2-10","2-11","2-12","2-13",
-"3-01","3-02","3-03","3-04","3-05","3-06","3-07","3-08","3-09","3-10","3-11","3-12","3-13",
-"4-01","4-02","4-03","4-04","4-05","4-06","4-07","4-08","4-09","4-10","4-11","4-12","4-13",
-"0"];
+let cardsArray=[];
+const cTI={
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10,
+    '11':11,
+    '12':12,
+    '13':13
+}
+const rTC={
+    "1":1,
+    "2":1,
+    "3":2,
+    "4":2,
+    "5":3,
+    "6":3,
+    "7":4,
+    "8":4,  
+}
+
+
 let deck=[];
 let handPlayer0=[];
 let handPlayer1=[];
@@ -17,9 +36,9 @@ let handPlayer2=[];
 let handPlayer3=[];
 let message=$("#message");
 let tableArray=[];
-
-
-
+const playerBoard= document.querySelectorAll('.board');
+var floor;
+//////////////////////verify//////////////////////////
 function verify(){
     n=$('input[name="num_of_players"]:checked').val();
     if(n==undefined){
@@ -32,25 +51,73 @@ function verify(){
     Start();
 };
 
+function updateDeck(){
+    $("#draw").empty();
+    let len= deck.length;
+    $("#draw").append("➕" +`<br>` +len);
+}
 
+/////////////////start//////////////////////////
 function Start(){
     console.log(n);
    for (i=2;i<=n;i++){
         $("#game").append(`<div id="player-${i-1}" class="board"></div>`);
         
    }
+   setDeck();
    Shuffle(cardsArray);
    deal_cards(n,deck);
-   makeTable();
-
+   turn();
+    //makeTable();
 };
 function makeTable(){
     for (i=0;i<78;i++)
     {
-        $("#floor").append(`<span id="card" draggable="true"></span>`);
+        $("#floor").append(`<span id="card" class="card" draggable="true"></span>`);
     }
 
 }
+function setDeck(){
+    let counter=0;
+ for (let i = 1; i <= 8; i++) {
+        for (let j = 1; j <= 13; j++) {
+            if (i%2 === 1) {
+               cardsArray.push({
+                    "id": `${i}${cTI[j]}`,
+                    "card": `<span id="${i}${cTI[j]}" class="card" style="background-image:url('/pics/${rTC[i]}-0${cTI[j]}.svg')" color="${rTC[i]}" number="${cTI[j]}"></span>`,
+                    "color": rTC[i],
+                    "number": `${cTI[j]}`
+                    
+                });
+               
+                
+            } else {
+                cardsArray.push({
+                    "id": `${i}${cTI[j]}`,
+                    "card": `<span id="${i}${cTI[j]}" class="card" draggable="true" style="background-image:url('/pics/${rTC[i]}-0${cTI[j]}.svg')" color="${rTC[i]}" number="${cTI[j]}"></span>`,
+                    "color": rTC[i],
+                    "number": `${cTI[j]}`
+                });
+                
+            }
+
+        }
+    }
+    cardsArray.push({
+        "id": 0,
+        "card": `<span id="0" class="card" style="background-image:url('/pics/0.svg')" color="0" number="0"></span>`,
+        "color": 0,
+        "number": `0`
+    });
+    cardsArray.push({
+        "id": 0,
+        "card": `<span id="0" class="card" style="background-image:url('/pics/0.svg')" color="0" number="0"></span>`,
+        "color": 0,
+        "number": `0`
+    });
+}
+
+
 function Shuffle(cardsArray){
     for(i=0;i<cardsArray.length;i++)
     {
@@ -69,29 +136,39 @@ function deal_cards(numberOfPlayers,Deck){
     {
         for(j=0;j<14;j++){
                 temp=Deck.pop();
+                let tileElement=$(temp.card)
             if (i==0)
             {
-                $("#player-0").append(`<span id="card" draggable="true"><img src="/pics/${temp}.svg"></span>`)
+                $("#player-0").append(tileElement)
                 handPlayer0.push(temp);
             }
             else if(i==1)
             {
-                $("#player-1").append(`<span id="card" draggable="true"><img src="/pics/${temp}.svg"></span>`)
+                $("#player-1").append(tileElement)
                 handPlayer1.push(temp);
             }
             else if (i==2)
             {
-                $("#player-2").append(`<span id="card" draggable="true"><img src="/pics/${temp}.svg"></span>`)
+                $("#player-2").append(tileElement)
                 handPlayer2.push(temp);
             }
             else
             {
-                $("#player-3").append(`<span id="card" draggable="true"><img src="/pics/${temp}.svg"></span>`)
+                $("#player-3").append(tileElement)
                 handPlayer3.push(temp);
             }
         }
     }
+   //dragNDrop();
+    //$(".board").sortable();
+    updateDeck();
 }
+
+
+
+
+////////////////////////////////in game//////////////////////////
+
 function draw(Deck){
     let temp
     if(deck.length==0){
@@ -100,25 +177,260 @@ function draw(Deck){
     else
     {
         temp=deck.pop();
-        $("#player-0").append(`<span id="card" draggable="true"><img src="/pics/${temp}.svg"></span>`)
+        let tileElement=$(temp.card)
+        $("#player-0").append(tileElement);
         handPlayer0.push(temp);
     }
+   //dragNDrop();
+   updateDeck();
 }
-
 
 function sort789()
 {
-    handPlayer0.sort();
+    
+    handPlayer0.sort(function(a,b){return parseInt(a.color)-parseInt(b.color)});
+
     $(`#player-0`).empty();
-    for(let card of handPlayer0){
-        $("#player-0").append(`<span id="card" draggable="true"><img src="/pics/${card}.svg"></span>`)
+    for(i=0;i<handPlayer0.length;i++){
+        temp=handPlayer0[i].card;
+        
+        $("#player-0").append(temp);
     }
+    turn();
+}
+ function sort777(){
+
+    handPlayer0.sort(function(a,b){return parseInt(a.number) -parseInt(b.number)});
+
+    $(`#player-0`).empty();
+    for(i=0;i<handPlayer0.length;i++){
+        temp=handPlayer0[i].card;
+       
+        $("#player-0").append(temp);
+    }
+    turn();
 }
 
-// function sort777()
-// {
+const screenShot={
+    //"computerHand":[],//cards in array (stirngs)
+    "playerHand":[],//cards in array (stirngs)
+    "board":[],//cards in array (stirngs)
+    "remainDeck":[]//cards in array (stirngs)
+}
+
+//deep copy each array from screenShot
+const deepClone=(target,arr)=>{
+    for(let i=0;i<arr.length;i++){
+        target.push(arr[i]);
+    }
+}
    
+const snap=()=>{
+    deepClone(screenShot.playerHand,playerHand);
+    deepClone(screenShot.board,board);
+    deepClone(screenShot.remainDeck,remainDeck);
+}
+
+ $(function(){
+     $(".floor").sortable();
+
+ });
+//  $(function(){
+//     $(".board").sortable();
+
+// });
+
+function turn(){
+
+
+    //  $( ".board" ).sortable({
+    //      revert: true
+    //    });
+    $( ".board" ).draggable({
+        //helper:"clone",
+        //  connectToSortable: ".board",
+         drag:function(){
+            console.log('drag');
+         }
+    });
+      $( "div, span" ).disableSelection();
+}
+    // $(".floor").droppable({
+    //      drop:function(event,ui){
+    //          var position=ui.position;
+    //          var cellX=calculaceCellX(position.left);
+    //          var cellY=calculaceCellY(position.top);
+
+    //          moveCardToCell(ui.draggable, cellX, cellY);
+    //           tableArray.push($(this));
+    //           moveCard(ui.draggable,$(this))
+
+    //          moveCard(tableArray)
+    //      }
+        
+    //});
+
+//     function calculateCellX(positionLeft) {
+//         var relativeX = positionLeft - floorOffsetLeft; 
+//         return Math.floor(relativeX / cellSize); 
+//     }
+    
+//     function calculateCellY(positionTop) {
+//         var relativeY = positionTop - floorOffsetTop; 
+//         return Math.floor(relativeY / cellSize); 
+//     }
+    
+//     function moveCardToCell(cardDiv, cellX, cellY) {
+//         cardDiv.detach();
+//         cardDiv.css({ 
+//             position: 'absolute', 
+//             left: (cellX * cellSize) + 'px',  
+//             top: (cellY * cellSize) + 'px' 
+//         });
+//         cardDiv.appendTo('#floor'); 
+//     }
+
 // }
+
+// function moveCard(cardDiv, destinationDiv) {
+//     // Detach the cardDiv (removes it but preserves data)
+//     cardDiv.detach(); 
+
+//     // Append the cardDiv to the destination
+//     destinationDiv.appendTo(destinationDiv);  
+// }
+
+
+
+
+
+
+// function renderFloor(Array){
+//     console.log(Array);
+//     $(".floor").empty();
+//     for(var i in Array){
+//         var node = Array [i]
+//         console.log(Array);
+//         $(".floor").append("<span>somthing</span>");
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+//////handeling dragging//////
+
+
+
+
+
+
+
+
+
+
+// const makeCardDragable=()=>{
+//     console.log('makeCardDragable!');
+//     $('#card').on('drag',function(){
+//         console.log("#card is in");
+//         console.log(this);
+//         draggingDiv=$(this);
+//         console.log("draggingDiv",DraggingDiv);
+
+//     })
+    
+// }
+
+
+
+
+
+// playerBoard.forEach(board=>{
+//     board.addEventListener('dragstart',handleDragStart);
+//     board.addEventListener('dragend',handleDragEnd);
+// })
+
+// function handleDragStart(event){
+//     event.dataTransfer.setData('text',event.target.id);
+//     console.log('tileId:', event.target.id);
+//     event.target.style.opacity='0.5';
+// }
+
+
+// function handleDragEnd(event){
+//    event.target.style.opacity='1';
+// }
+
+// floor= document.getElementById('floor')
+//     floor.addEventListener('dragover',handleDragOver);
+//     floor.addEventListener('drop',handleDrop);
+
+// function handleDragOver(event)
+// {
+//     event.preventDefault();
+// }
+
+// function handleDrop(event){
+//     event.preventDefault();
+//     const tileId=event.dataTransfer.getData('text/plain');
+//     const tile=document.getElementById(tileId);
+//     console.log('tileId: ', tileId);
+//     console.log('tile: ', tile);
+
+//     if (event.target.id === 'floor') { // Check if the drop target is the "floor"
+//         event.target.appendChild(tile); 
+//     }
+// }
+
+
+
+/////////////exit///////////////
+function exit(){
+    $("#game").css("visibility","hidden")
+    $("#main").css("visibility","visible")
+    $("#player-1").remove();
+    $("#player-2").remove();
+    $("#player-3").remove();
+    fc = document.getElementById('player-0');
+    while (fc.firstChild) {
+        fc.removeChild(fc.firstChild);
+    }
+    deck=[];
+    fb=document.getElementById('floor')
+    while(fb.firstChild){
+        fb.removeChild(fb.firstChild)
+    }
+    cardsArray=[];
+    handPlayer0=[];
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // document.addEventListener('DOMContentLoaded', function() {
 //     const card=document.getElementById('card');
 //     let table=document.getElementById('floor');
@@ -136,22 +448,9 @@ function sort789()
 //     })
 
 
-// });
+//  });
 
 // function drag(card){
 //     let card=
 
 // }
-
-function exit(){
-    $("#game").css("visibility","hidden")
-    $("#main").css("visibility","visible")
-    $("#player-1").remove();
-    $("#player-2").remove();
-    $("#player-3").remove();
-    fc = document.getElementById('player-0');
-    while (fc.firstChild) {
-        fc.removeChild(fc.firstChild);
-    }
-    deck=[];
-}
